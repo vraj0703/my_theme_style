550b37e2a04d9f72a97b60c62b36fdbd5fa7223a
vraj0703 <vraj0703@gmail.com>
2025-10-09 23:21:15 +0530

    remove asset manager

--- lib/library.dart
+++ lib/library.dart
@@ -1,6 +1,5 @@
 library;
 
-export 'package:my_theme_style/assets.dart';
 export 'package:my_theme_style/styles/colors.dart';
 export 'package:my_theme_style/styles/styles.dart';
 export 'package:my_theme_style/utils/color_utils.dart';
